9b442873c97e38ffab5143cc39eedd2d920e4e01
Util787 <georgiy.chuprina.787@mail.ru>
2025-06-25 02:25:18 +0300

    feat: add config

---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+	"log/slog"
+
+	http_server "github.com/Util787/task-manager/pkg/http-server"
+	"github.com/caarlos0/env/v11"
+)
+
+type Config struct {
+	Env        string `env:"ENV" envDefault:"prod"`
+	HttpServer http_server.Config
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{}
+
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	slog.Info("Configuration loaded successfully", // for debug, delete later
+		"env", cfg.Env,
+		"http_port", cfg.HttpServer.Port,
+		"http_read_header_timeout", cfg.HttpServer.ReadHeaderTimeout,
+		"http_write_timeout", cfg.HttpServer.WriteTimeout,
+		"http_read_timeout", cfg.HttpServer.ReadTimeout,
+	)
+
+	return cfg, nil
+}
