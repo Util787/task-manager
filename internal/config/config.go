5fe6fc2799531be5570f89d4c6956f7c06f7d386
Util787 <georgiy.chuprina.787@mail.ru>
2025-06-28 03:09:05 +0300

    refactor: delete info log in config to improve privacy

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"log/slog"
 
 	http_server "github.com/Util787/task-manager/pkg/http-server"
 	"github.com/caarlos0/env/v11"
@@ -10,7 +9,7 @@ import (
 )
 
 type Config struct {
-	Env        string `env:"ENV" envDefault:"prod"`
+	Env           string `env:"ENV" envDefault:"prod"`
 	HttpServerCfg http_server.Config
 }
 
@@ -28,14 +27,6 @@ func Load() (*Config, error) {
 	if cfg.Env != "prod" && cfg.Env != "dev" && cfg.Env != "local" {
 		return nil, fmt.Errorf("invalid environment: %s, must be prod, dev or local", cfg.Env)
 	}
-
-	slog.Info("Configuration loaded successfully", // for debug, delete later
-		"env", cfg.Env,
-		"http_port", cfg.HttpServerCfg.Port,
-		"http_read_header_timeout", cfg.HttpServerCfg.ReadHeaderTimeout,
-		"http_write_timeout", cfg.HttpServerCfg.WriteTimeout,
-		"http_read_timeout", cfg.HttpServerCfg.ReadTimeout,
-	)
 
 	return cfg, nil
 }
